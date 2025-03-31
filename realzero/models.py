0f4215b09e90b066c02b72642d0e4a8e905736f6
98donggyu <98donggyu@gmail.com>
2025-03-31 09:13:52 +0900

    서비스 초안

--- realzero/models.py
+++ realzero/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class realzero(models.Model):
+    id = models.AutoField(primary_key=True)
+    Manufacturer = models.CharField(default="Unknown", max_length=50, verbose_name="제조사명")
+    product_name = models.CharField(max_length=50, verbose_name="제품명")
+    Capacity =  models.CharField(max_length=50, default=0, verbose_name="용량 (g)")
+    Product_calorific_value = models.CharField(max_length=50, default=0, verbose_name="열량 (g)")
+    carbohydrates = models.CharField(max_length=50, verbose_name="탄수화물 (g)")
+    protein =models.CharField(max_length=50, verbose_name="단백질 (g)")
+    fat = models.CharField(max_length=50, verbose_name="지방 (g)")
+    saturated_fat = models.CharField(max_length=50, default=0, verbose_name="포화지방 (g)")
+    trans_fat = models.CharField(max_length=50, default=0, verbose_name="트렌스지방 (g)")
+    Cholesterol = models.CharField(max_length=50, default=0, verbose_name="콜레스테롤 (mg)")
+    sodium = models.CharField(max_length=50, default=0, verbose_name="나트륨 (mg)")
+    Caffeine = models.CharField(max_length=50, default=0,verbose_name="카페인 (g)")
+    sugar = models.CharField(max_length=50, default=0, verbose_name="당류 (g)")
+    Sugar_alcohol =  models.CharField(max_length=50, default=0, verbose_name="당알콜 (g)")
+    maltitol = models.CharField(max_length=50, default=0, verbose_name="말티톨 (g)")
+    maltitol_syrup = models.CharField(max_length=50, default=0, verbose_name="말티톨 시럽 (g)")
+    glucose_syrup = models.CharField(max_length=50, default=0, verbose_name="물엿 (g)")
+    GI = models.CharField(max_length=50, default=0, verbose_name="GI지수")
+    GL = models.CharField(max_length=50, default=0, verbose_name="GL지수")
+    Raw_materials = models.TextField(default="Unknown", max_length=255, verbose_name="원재료명")
+    emoji = models.CharField(max_length=20, default="😊")
+    def __str__(self):
+        return self.product_name
+
+    class Meta:
+        verbose_name = "제로식품"
+        verbose_name_plural = "제로식품 목록"
---
+++ realzero/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('ranking/', views.ranking, name='ranking'),
+    path('community/', views.community, name='community'),
+    path('search/', views.search, name='search'),
+    path('product/<int:id>/', views.product_detail, name='product_detail'),
+]
