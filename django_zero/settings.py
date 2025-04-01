fd1745cc8e466068e7d82190ab03e01ab0a7fd38
98donggyu <98donggyu@gmail.com>
2025-04-01 16:17:11 +0900

    readme파일수정

--- django_zero/settings.py
+++ django_zero/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8ywh06^lkn+&98%wtz+@r1vzvzsl!p5k^(6cpzooyz3p9yd0%4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.101.67']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.101.67']
 
 
 # Application definition
